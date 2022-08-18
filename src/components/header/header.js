af1f092fc9ca7ccfbec1a2c3322aa25ea6684d94
Kacper Kwiatkowski <kacper18k@gmail.com>
2022-08-18 14:42:02 +0200

    Init commit

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+
+
+const Header = () => {
+
+    return(
+        <header>
+            <ul>
+                <li>LOGIN</li>
+                <li>REGISTER</li>
+                <li>POST TWEET</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
