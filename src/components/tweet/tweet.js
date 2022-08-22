d47f8eddd9e759920ab4bc3eb496a88ba8566527
Kacper Kwiatkowski <kacper18k@gmail.com>
2022-08-22 13:36:01 +0100

    Tweets can be liked

--- src/components/tweet/tweet.js
+++ src/components/tweet/tweet.js
@@ -2,9 +2,30 @@ import "./tweet-style.scss"
 
 import React, {useEffect, useState} from "react";
 
+import Axios from "axios";
+import "../../interceptors/authTokenProvider"
+
 const Tweet = ({tweet, allowReply}) => {
 
     const [date, setDate] = useState(new Date());
+    const [loggedUserData, setLoggedUserData] = useState({})
+
+
+    useEffect(() => {
+        fetchLoggedUser()
+    }, loggedUserData);
+
+
+    const fetchLoggedUser = () => {
+        Axios.get("http://localhost:8080/api/v.1.0/tweets/logged")
+            .then(response => {
+                if (response.status === 200) {
+                    setLoggedUserData(response.data)
+                }
+            }).catch(error => {
+            console.error(error)
+        })
+    }
 
     useEffect(() => {
         let timer = setInterval(() => setDate(new Date()), 100000);
@@ -12,6 +33,18 @@ const Tweet = ({tweet, allowReply}) => {
             clearInterval(timer)
         }
     });
+
+
+    const handleLikeTweet = () => {
+
+        Axios.put(`http://localhost:8080/api/v.1.0/tweets/${loggedUserData.username}/like/${tweet.tweetId}`,
+        )
+            .then(response => {
+
+            }).catch(error => {
+            console.error(error)
+        })
+    }
 
     return (
         <div className="tweet">
@@ -21,7 +54,7 @@ const Tweet = ({tweet, allowReply}) => {
                 <div className="tweetUserDetails">
 
                     <div className="userDetails">{tweet.username}</div>
-                    <div className="userDetails">{tweet.firstNa} {tweet.username}</div>
+                    <div className="userDetails">{tweet.firstName} {tweet.lastName}</div>
                     <div
                         className="userDetails currentTime">{tweet.postDateTime}</div>
                 </div>
@@ -30,7 +63,9 @@ const Tweet = ({tweet, allowReply}) => {
                     <div className="userDetails">{tweet.message}</div>
                 </div>
                 <div className="buttonWrapper">
-                    <button className="formButton" type="submit" value="Submit">Like</button>
+
+                    <div className="userDetails">{tweet.likeCount}</div>
+                    <button className="formButton" type="submit" value="Submit" onClick={handleLikeTweet}>Like</button>
                 </div>
             </div>
         </div>
