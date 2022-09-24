55e1ffdbcbb1b5b59ddbc7acf334a7f571b14ef8
Kacper Kwiatkowski <kacper18k@gmail.com>
2022-09-24 13:45:10 +0200

    Error fix

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-server.keepAliveTimeout = 65000;
-server.headersTimeout = 80000;
-
 root.render(
   <React.StrictMode>
       <App />
